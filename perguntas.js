31100ecc3f6544405cf32ad45c62c5c25883772b
darienHR <darienA.hernandez15@gmail.com>
2024-11-30 18:01:28 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Geografia',
+    'Qual è o maior paìs do mundo?',
+    'A Russia com 17 075 400 km²'
+)
+
+criaCartao(
+    'Historia',
+    'Qual o primeiro homem que chegou na lua?',
+    'O americano Neil Armstrong em 1961'
+)
+
+criaCartao(
+    'Matemàtica',
+    'Qual è a raìz quadrada de 169?',
+    '13'
+)
+
+criaCartao(
+    'Biologia',
+    'Qual è o orgao responsàvel pelo equilibrio?',
+    'O cerebelo, cituado no sistema nervoso do ser humano'
+)
